558397a3bf49acdacac78ac1f38fd3f903fa15a7
gabrielravanhan <gabrielravanhanjose@gmail.com>
2020-11-21 21:10:42 -0300

    21-11-2020
    Finalização da tarefa

---
+++ Aula_17-09-2020/js/script.js
@@ -0,0 +1,59 @@
+$('#fundoPrimary').click(function () {
+    $('body').addClass('bg-primary');
+});
+
+$('#fundoSecondary').click(function () {
+    $('body').addClass('bg-secondary');
+});
+
+$('#fundoSuccess').click(function () {
+    $('body').addClass('bg-success');
+});
+
+$('#fundoInfo').click(function () {
+    $('body').addClass('bg-info');
+});
+
+$('#fundoWarning').click(function () {
+    $('body').addClass('bg-warning');
+});
+
+$('#fundoDanger').click(function () {
+    $('body').addClass('bg-danger');
+});
+
+$('#fundoLight').click(function () {
+    $('body').addClass('bg-light');
+});
+
+$('#fundoDark').click(function () {
+    $('body').addClass('bg-dark');
+});
+
+$('#ocultarFilmes').click(function () {
+    $('#filmes').hide();
+});
+
+$('#exibirFilmes').click(function () {
+    $('#filmes').show();
+});
+
+$('#ocultarSeries').click(function () {
+    $('#series').hide();
+});
+
+$('#exibirSeries').click(function () {
+    $('#series').show();
+});
+
+$('#ocultarDesenhos').click(function () {
+    $('#desenhos').hide();
+});
+
+$('#exibirDesenhos').click(function () {
+    $('#desenhos').show();
+});
+
+$('#enviar').click(function(){
+    alert("Nome: " + $('#nome').val() +"\nE-mail: " + $('#email').val() +"\nMensagem: " + $('#mensagem').val());
+});
